33d1863d5d42a6e1df71aefb239f1ab601197065
szy00789 <qq870851932@163.com>
2020-12-08 16:31:29 +0800

    Initial commit

---
+++ src/main/java/com/qf/entity/User.java
@@ -0,0 +1,15 @@
+package com.qf.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String name;
+    private String password;
+    private int age;
+}
